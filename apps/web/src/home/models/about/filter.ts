683c28ead08b1bd52afde04f90b0f5768bc00ac6
Rdani2005 <daniel.sequeira@betanysports.eu>
2025-05-22 09:08:23 -0600

    refactor: using valibot for WithTitle

--- apps/web/src/home/models/about/filter.ts
+++ apps/web/src/home/models/about/filter.ts
@@ -1,4 +1,10 @@
-type WithTitle = { title: string };
+import { InferOutput, object, string } from "valibot";
+
+const WithTitle = object({
+  title: string(),
+});
+
+type WithTitle = InferOutput<typeof WithTitle>;
 
 export function filterByTitle<T extends WithTitle>(
   items: T[],
